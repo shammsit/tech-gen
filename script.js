664d6128ade3f255f20eedb94047235f40c37077
SHAMANTA GARAI <shamanta_gaids2024@msit.edu.in>
2026-02-20 14:04:56 +0530

    Add dark mode and mobile menu toggle functions

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 function openSafeLink(){
     window.open("https://fascinating-crostata-0627c3.netlify.app/", "_blank");
 }
+
+// Dark Mode
+function toggleDark(){
+    document.body.classList.toggle("dark");
+}
+
+// Mobile Menu
+function toggleMenu(){
+    document.getElementById("navLinks").classList.toggle("show");
+}
