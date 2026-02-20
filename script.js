6cd134535e8ceda7344822322fa7d510401638f7
SHAMANTA GARAI <shamanta_gaids2024@msit.edu.in>
2026-02-20 14:42:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
 function openSafeLink(){
     window.open("https://fascinating-crostata-0627c3.netlify.app/", "_blank");
 }
-
-// Dark Mode
-function toggleDark(){
-    document.body.classList.toggle("dark");
-}
-
-// Mobile Menu
-function toggleMenu(){
-    document.getElementById("navLinks").classList.toggle("show");
-}
